bb8cf7ed030272a71de9c570ea9644b36aed24a7
lillyanrezende <lillyanaloisemkt@gmail.com>
2025-01-05 14:17:01 +0000

    adicionado espaço administrador

--- projeto_gestaopessoal/projeto_gestaopessoal/urls.py
+++ projeto_gestaopessoal/projeto_gestaopessoal/urls.py
@@ -1,11 +1,14 @@
+from django.contrib import admin
 from django.urls import path
 from app_gestaopessoal import views
 
 urlpatterns = [
+ path('admin/', admin.site.urls),
  # rota, view responsável, nome de referência
  # estrategiandomarketing.com
  path('', views.home, name='home'),
  # estrategiandomarketing.com/usuarios
  path('usuarios/', views.usuarios, name='listagem_usuarios')
+ 
 
 ]
